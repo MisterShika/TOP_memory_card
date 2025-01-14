6a75ccb2b9dd2cf2372df3a76e2ad29c50884194
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-01-14 22:05:11 +0900

    Some styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Primary from './Primary.jsx'
+import './style/reset.css'
+import './style/style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
